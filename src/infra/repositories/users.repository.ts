5e15522244b3b44c43d5a4a8475252dd25277916
Miguel Pombo <miguel.pombocontractor@modec.com>
2024-02-01 11:54:21 -0300

    feat: installing eslint, prettier and editorconfig in project

--- src/infra/repositories/users.repository.ts
+++ src/infra/repositories/users.repository.ts
@@ -3,18 +3,21 @@ import { Users } from "../../domain/entities/Users";
 import { dbConnectInitiator } from "../database/connections";
 import { IUsersRepository } from "../../domain/interfaces/repositories/usersRepository.interface";
 
-export default class UsersRepository implements IUsersRepository{
-  private repository: Repository<Users>
+export default class UsersRepository implements IUsersRepository {
+	private repository: Repository<Users>;
 
-  constructor () {
-    this.repository = dbConnectInitiator.relationalDatabaseConnectionORMOptions.getRepository(Users)
-  }
+	constructor() {
+		this.repository =
+			dbConnectInitiator.relationalDatabaseConnectionORMOptions.getRepository(
+				Users,
+			);
+	}
 
-  async findByIdAsync(id: string): Promise<Users | null> {
-    return await this.repository.findOne({
-      where: {
-        Id: id
-      }
-    })
-  }
+	async findByIdAsync(id: string): Promise<Users | null> {
+		return await this.repository.findOne({
+			where: {
+				Id: id,
+			},
+		});
+	}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-import 'reflect-metadata'
-import 'dotenv/config'
-import express, {Router, Request, Response} from 'express'
-import { userRouter } from './web/routes/users.route'
+import "reflect-metadata";
+import "dotenv/config";
+import express, { Router, Request, Response } from "express";
+import { userRouter } from "./web/routes/users.route";
 
-const app = express()
-const router = Router()
+const app = express();
+const router = Router();
 
-app.use(express.json())
+app.use(express.json());
 
-router.get('/', (req: Request, res: Response) => {
-  return res.json({
-    'message': "It's working!"
-  })
-})
+router.get("/", (req: Request, res: Response) => {
+	return res.json({
+		message: "It's working!",
+	});
+});
 
-app.use(router)
-app.use(userRouter)
+app.use(router);
+app.use(userRouter);
 
-app.listen(3333,() => {
-  console.log(`Server is running in port ${process.env.APP_PORT}`)
-})
+app.listen(3333, () => {
+	console.log(`Server is running in port ${process.env.APP_PORT}`);
+});
